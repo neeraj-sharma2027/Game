dd29a67658b58d0dabd56692733004108d38a921
neeraj-sharma2027 <neeraj2004sh@gmail.com>
2026-04-26 13:28:27 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,79 @@
+let gameseq =[];
+let userseq = [];
+let started = false;
+let level = 0;
+let btns = ["yellow","green","red","purple"]; 
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started == false){
+        console.log("game started ");
+        started == true;
+        levelup();
+    }
+});
+function gameflash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+}
+function userflash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },250);
+}
+
+
+function levelup(){
+    userseq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+    let randidx = Math.floor(Math.random()*3);
+    let randcolor = btns[randidx];
+    let randbtn = document.querySelector(`.${randcolor}`);
+    gameseq.push(randcolor);
+    console.log(gameseq);
+
+    gameflash(randbtn);
+}
+
+function checkans(idx){
+    //console.log("currlevel : ",level);
+    //let idx = level - 1;
+    if(userseq[idx] === gameseq[idx]){
+        //console.log("samevalue");
+        if(userseq.length == gameseq.length){
+            setTimeout(levelup(),1000);
+        }
+    }
+    else{
+        h2.innerHTML = `Game Over! Your Score was <b>${level}</br><br>Press any key to start. `;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        },150);
+        reset();
+    }
+}
+function btnpress(){
+    let btn = this;
+    userflash(btn);
+    usercolor = btn.getAttribute("id");
+    userseq.push(usercolor);
+    checkans(userseq.length-1);
+}
+ 
+let allbtns = document.querySelectorAll(".btn");
+for(btn of allbtns){
+    btn.addEventListener("click",btnpress);
+}
+
+function reset(){
+    started = false;
+    userseq = [];
+    gameseq = [];
+    level = 0;
+}
